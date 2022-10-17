54a245a74dd3db8dbbf7b347a73719ca33245aea
Sasha <hlupik69@gmail.com>
2022-10-18 02:08:01 +0300

    docs: fix solution binary-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -32,7 +32,7 @@ class BinarySearchTree {
     if (_root.data === value.data) {
       return;
     } else if (_root.data > value.data && _root.left != null) {
-      this.addNode(root.left, value)
+      this.addNode(_root.left, value)
       return
     } else if (_root.data < value.data && _root.right != null) {
       this.addNode(_root.right, value)
@@ -48,13 +48,44 @@ class BinarySearchTree {
     }
   }
   has(data) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return check(this._root, data);
+
+    function check(_root, data) {
+      if (!_root) {
+        return false;
+      }
+
+      if (_root.data === data) {
+        return true;
+      }
+
+      if (_root.data < data) {
+        return check(_root.right, data);
+      } else {
+        return check(_root.left, data);
+      }
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  find(data) {
+    return check(this._root, data);
+
+    function check(_root, data) {
+      if (!_root) {
+        return null;
+      }
+
+      if (_root.data === data) {
+        return _root;
+      }
+
+      if (_root.data < data) {
+        return check(_root.right, data);
+      } else {
+        return check(_root.left, data);
+      }
+    }
   }
 
   remove(/* data */) {
@@ -63,16 +94,16 @@ class BinarySearchTree {
   }
 
   min(_root = this._root) {
-    if (_root.left != null) return this.min(root.left)
+    if (_root.left != null) return this.min(_root.left)
     else return _root.value;
   }
 
   max(_root = this._root) {
-    if (_root.right != null) return this.max(root.right)
-    else return _root.data;
+    if (_root.right != null) return this.max(_root.right)
+    else return _root.value;
   }
-}
 
+}
 module.exports = {
   BinarySearchTree
 };
